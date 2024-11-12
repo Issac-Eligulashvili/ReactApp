a88506d18b07435a683f3c06d19ac840903a72b2
Issac-Eligulashvili <issac.eli2007@gmail.com>
2024-11-12 12:26:45 -0500

    started

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LinearGradient
+        colors={["#2E1A47", "#0B1124"]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        style={styles.gradient} // Add a style for LinearGradient
+      >
+        <Text style={styles.text}>
+          Open up App.tsx to start working on your app!
+        </Text>
+        <StatusBar style="auto" />
+      </LinearGradient>
     </View>
   );
 }
@@ -13,8 +23,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  gradient: {
+    flex: 1, // This makes the gradient cover the full area
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%", // Full width to match the parent container
+  },
+  text: {
+    color: "#fff",
+    fontSize: 16,
   },
 });
